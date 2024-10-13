07a4ed86670f41a550125b5baa469fa72d30c7b4
v.varabei <vitalyvorobej@gmail.com>
2024-10-14 01:31:00 +0300

    initial commit

---
+++ src/main/java/com/course/project/varabei/logger/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,7 @@
+package com.course.project.varabei.logger;
+
+public class LoggingStarterAutoConfiguration {
+    public static void println(String message) {
+        System.out.println(message);
+    }
+}
